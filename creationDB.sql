7042733d9dfa7e3c9279143aa66d18818ec2bf21
Christian Jiwan <christian@cjiwan.fr>
2022-03-20 18:01:43 +0100

    Création des Tables
    Fichier Création des Tables/Liaisons
    Corrigé avec la nouvelle structure

--- creationDB.sql
+++ creationDB.sql
@@ -7,7 +7,7 @@ USE cineStudi;
 #
 CREATE TABLE IF NOT EXISTS Administrators
 (
-    id           VARCHAR(36)  NOT NULL PRIMARY KEY,
+    id           VARCHAR(36) NOT NULL PRIMARY KEY,
     name         VARCHAR(32) NOT NULL,
     email        VARCHAR(32) NOT NULL,
     password     BINARY(64)  NOT NULL,
@@ -15,25 +15,25 @@ CREATE TABLE IF NOT EXISTS Administrators
 );
 CREATE TABLE IF NOT EXISTS Complexes
 (
-    id      VARCHAR(36)   NOT NULL PRIMARY KEY,
+    id      VARCHAR(36)  NOT NULL PRIMARY KEY,
     name    VARCHAR(128) NOT NULL,
     address VARCHAR(255) NOT NULL
 );
 CREATE TABLE IF NOT EXISTS Rooms
 (
     id          VARCHAR(36) NOT NULL PRIMARY KEY,
-    number      INTEGER    NOT NULL,
-    seatNumner  INTEGER    NOT NULL,
+    number      INTEGER     NOT NULL,
+    seatNumner  INTEGER     NOT NULL,
     description VARCHAR(255)
 );
 CREATE TABLE IF NOT EXISTS Seances
 (
     id        VARCHAR(36) NOT NULL PRIMARY KEY,
-    startTime TIME       NOT NULL
+    startTime TIME        NOT NULL
 );
 CREATE TABLE IF NOT EXISTS Films
 (
-    id          VARCHAR(36)  NOT NULL PRIMARY KEY,
+    id          VARCHAR(36) NOT NULL PRIMARY KEY,
     name        VARCHAR(64) NOT NULL,
     description VARCHAR(255),
     duration    INTEGER     NOT NULL,
@@ -41,19 +41,19 @@ CREATE TABLE IF NOT EXISTS Films
 );
 CREATE TABLE IF NOT EXISTS Tarifs
 (
-    id          VARCHAR(36)   NOT NULL PRIMARY KEY,
+    id          VARCHAR(36)  NOT NULL PRIMARY KEY,
     description VARCHAR(128) NOT NULL,
     price       INTEGER      NOT NULL
 );
 CREATE TABLE IF NOT EXISTS Reservations
 (
     id      VARCHAR(36) NOT NULL PRIMARY KEY,
-    prePaid BOOLEAN    NOT NULL,
-    isPaid  BOOLEAN    NOT NULL
+    prePaid BOOLEAN     NOT NULL,
+    isPaid  BOOLEAN     NOT NULL
 );
 CREATE TABLE IF NOT EXISTS Customers
 (
-    id      VARCHAR(36)  NOT NULL PRIMARY KEY,
+    id      VARCHAR(36) NOT NULL PRIMARY KEY,
     name    VARCHAR(32) NOT NULL,
     address VARCHAR(255),
     email   VARCHAR(32) NOT NULL
@@ -66,12 +66,8 @@ CREATE TABLE IF NOT EXISTS Customers
 ALTER TABLE Administrators
     ADD complexId VARCHAR(36),
     ADD FOREIGN KEY (complexId) REFERENCES Complexes (id);
-# Relation Reservations <-> Customers
-ALTER TABLE Reservations
-    ADD customerId VARCHAR(36) NOT NULL,
-    ADD FOREIGN KEY (customerId) REFERENCES Customers (id);
 # Relation Complexes <-> Rooms
-CREATE TABLE IF NOT EXISTS Complexes_Rooms
+CREATE TABLE Complexes_Rooms
 (
     complexeId VARCHAR(36) NOT NULL,
     roomId     VARCHAR(36) NOT NULL,
@@ -79,25 +75,31 @@ CREATE TABLE IF NOT EXISTS Complexes_Rooms
     FOREIGN KEY (complexeId) REFERENCES Complexes (id),
     FOREIGN KEY (roomId) REFERENCES Rooms (id)
 );
-# Relation Rooms <-> Séances
-CREATE TABLE IF NOT EXISTS Rooms_Seances
-(
-    roomId   VARCHAR(36),
-    seanceId VARCHAR(36),
-    PRIMARY KEY (roomId, seanceId),
-    FOREIGN KEY (roomId) REFERENCES Rooms (id),
-    FOREIGN KEY (seanceId) REFERENCES Seances (id)
-);
-# Relation Seance <-> Films
-CREATE TABLE IF NOT EXISTS Seances_Films
-(
-    seanceId VARCHAR(36),
-    filmsId  VARCHAR(36),
-    PRIMARY KEY (seanceId, filmsId),
-    FOREIGN KEY (seanceId) REFERENCES Seances (id),
-    FOREIGN KEY (filmsId) REFERENCES Films (id)
-);
-# Relation Reservations <-> Seances
+# Relation Films <-> Rooms
+ALTER TABLE Rooms
+    ADD filmId VARCHAR(36) NOT NULL,
+    ADD FOREIGN KEY (filmId) REFERENCES Films (id);
+#Relation Films <-> Séances
+ALTER TABLE Seances
+    ADD filmId VARCHAR(36) NOT NULL,
+    ADD FOREIGN KEY (filmId) REFERENCES Films (id);
+#Relation Rooms_Reservations
 ALTER TABLE Reservations
-    ADD seanceId VARCHAR(36) NOT NULL,
-    ADD FOREIGN KEY (seanceId) REFERENCES Seances (id);
+    ADD roomId VARCHAR(36) NOT NULL,
+    ADD FOREIGN KEY (roomId) REFERENCES Rooms (id);
+#Relation Seance <-> Reservation
+#Relation Customer <-> Reservation
+ALTER TABLE Reservations
+    ADD seanceId   VARCHAR(36) NOT NULL,
+    ADD FOREIGN KEY (seanceId) REFERENCES Seances (id),
+    ADD customerId VARCHAR(36) NOT NULL,
+    ADD FOREIGN KEY (customerId) REFERENCES Customers (id);
+#Relation Tarif <-> Reservation
+CREATE TABLE IF NOT EXISTS tarif_reservation
+(
+    tarifId       VARCHAR(36) NOT NULL,
+    reservationId VARCHAR(36),
+    PRIMARY KEY (tarifId, reservationId),
+    FOREIGN KEY (tarifId) REFERENCES Tarifs (id),
+    FOREIGN KEY (reservationId) REFERENCES Reservations (id)
+);
